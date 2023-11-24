a8d9c660f51715ad8faacbd5e17c2eed4aab4ca0
Abhirama Sonny <abhirama.sonny@gmail.com>
2023-11-24 16:32:40 -0600

    fps among other things

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
-from cloth.cloth_detection import Draw_Bounding_Box,Detect_Clothes,Load_DeepFashion2_Yolov3
+import time
+from cloth.cloth_detection import Draw_Bounding_Box, Detect_Clothes, Load_DeepFashion2_Yolov3
 from gun import YoloObjD, run
 import tensorflow as tf
+
 def main():
     weight_path_gun = 'yolo-obj_last.weights'
     config_path_gun = 'gun.cfg'
@@ -12,25 +14,21 @@ def main():
     cam1 = cv2.VideoCapture(0)
     cam2 = cv2.VideoCapture(1)
     cameras = [
-        (cam1, "cam 1", {"fps": 20, "width": 640, "height": 416}),
-        (cam2, "cam 2", {"fps": 20, "width": 640, "height": 416})
+        (cam1, "cam 1"),
+        (cam2, "cam 2")
     ]
-
-    for cap, window_name, properties in cameras:
-        cap.set(cv2.CAP_PROP_FPS, properties["fps"])
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, properties["width"])
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, properties["height"])
 
     run_gun_detection(yolo_detector, cameras)
 
 def run_gun_detection(yolo_detector, cameras):
     consecutive_gun_detected_count = 0
     buffer_iteration_count = 4
+    start_time = time.time()
 
     while True:
         combined_frames = []
 
-        for cam, window_name, properties in cameras:
+        for cam, window_name in cameras:
             ret, frame, boxes = run(yolo_detector, cam, window_name)
             if not ret:
                 continue
@@ -42,8 +40,14 @@ def run_gun_detection(yolo_detector, cameras):
 
         combined_frames_resized = [(cv2.resize(frame, None, fx=2, fy=2), boxes) for frame, boxes in combined_frames]
         combined_frame = np.hstack([frame for frame, _ in combined_frames_resized])
+
+        # FPS maths
+        elapsed_time = time.time() - start_time
+        fps = len(cameras) / elapsed_time
+        cv2.putText(combined_frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
         cv2.imshow("Combined Cameras", combined_frame)
-        
+
         gun_detected_this_iteration = any(boxes > 0 for _, boxes in combined_frames_resized)
         print(f"{gun_detected_this_iteration}, {[boxes for _, boxes in combined_frames_resized]}")
 
@@ -67,17 +71,23 @@ def run_gun_detection(yolo_detector, cameras):
 def run_clothes_detection(cameras):
     models = [Load_DeepFashion2_Yolov3() for _ in range(len(cameras))]
     cap_list = [cv2.VideoCapture(i) for i in range(len(cameras))]
-
+    start_time = time.time()
+    
     while True:
+        
         for i, cap in enumerate(cap_list):
             ret, frame = cap.read()
             if not ret:
-                print(f"Failed to capture image from camera {i}")
+                print(f"falure on cam{i}")
                 break
+            
 
             img_tensor = tf.convert_to_tensor(frame[tf.newaxis, ...], dtype=tf.float32) / 255.0
             img_with_boxes = Draw_Bounding_Box(frame, Detect_Clothes(img_tensor, models[i]))
-            cv2.imshow(f"Clothes detection Camera {i+1}", img_with_boxes)
+            elapsed_time = time.time() - start_time
+            fps = len(cameras) / elapsed_time
+            cv2.putText(img_with_boxes, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow(f"Clothes detection cam {i+1}", img_with_boxes)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
