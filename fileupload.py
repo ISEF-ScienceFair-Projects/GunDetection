24bef3d5d65c8f4cffdebd0d7d37a838da5b92ff
MarchEgg <146690147+MarchEgg@users.noreply.github.com>
2023-11-25 17:03:52 -0600

    Update fileupload.py

--- fileupload.py
+++ fileupload.py
@@ -1,5 +1,9 @@
-import os
 import boto3
 
-s3 = boto3.resource('s3')
-s3.meta.client.upload_file('/Users/abhiramasonny/Developer/Python/shourya project/use/frame.jpg','sb-project-files','my-images/frame.jpg') 
+import boto3
+session = boto3.Session(
+    aws_access_key_id=str(open("Sercet Stuff - Raami DONT PUSH TO GIT HUB\AWS_access.txt", "r").read()),
+    aws_secret_access_key=str(open("Sercet Stuff - Raami DONT PUSH TO GIT HUB\AWS_sec.txt", "r").read()),
+)
+s3 = session.resource('s3')
+s3.meta.client.upload_file(Filename='gunImages\gunMan.jpg', Bucket='sb-project-files', Key='gun_image.jpg')
