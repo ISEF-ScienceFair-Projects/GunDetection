9758c4cbca6b66342107b31ae2de12be161957c8
MarchEgg <146690147+MarchEgg@users.noreply.github.com>
2023-11-26 19:26:21 -0600

    Replacement for Upload.py and Twilio.py

---
+++ Gateway Texting/main.py
@@ -0,0 +1,106 @@
+import email, smtplib, ssl
+from providers import PROVIDERS
+
+# used for MMS
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+from os.path import basename
+
+def send_sms_via_email(
+    number: str,
+    message: str,
+    provider: str,
+    sender_credentials: tuple,
+    subject: str = "sent using etext",
+    smtp_server: str = "smtp.gmail.com",
+    smtp_port: int = 465,
+):
+    sender_email, email_password = sender_credentials
+    receiver_email = f'{number}@{PROVIDERS.get(provider).get("sms")}'
+
+    email_message = f"Subject:{subject}\nTo:{receiver_email}\n{message}"
+
+    with smtplib.SMTP_SSL(
+        smtp_server, smtp_port, context=ssl.create_default_context()
+    ) as email:
+        email.login(sender_email, email_password)
+        email.sendmail(sender_email, receiver_email, email_message)
+
+def send_mms_via_email(
+    number: str,
+    message: str,
+    file_path: str,
+    mime_maintype: str,
+    mime_subtype: str,
+    provider: str,
+    sender_credentials: tuple,
+    subject: str = "sent using etext",
+    smtp_server: str = "smtp.gmail.com",
+    smtp_port: int = 465,
+):
+
+    sender_email, email_password = sender_credentials
+    receiver_email = f'{number}@{PROVIDERS.get(provider).get("mms")}'
+
+    email_message=MIMEMultipart()
+    email_message["Subject"] = subject
+    email_message["From"] = sender_email
+    email_message["To"] = receiver_email
+
+    email_message.attach(MIMEText(message, "plain"))
+
+    with open(file_path, "rb") as attachment:
+        part = MIMEBase(mime_maintype, mime_subtype)
+        part.set_payload(attachment.read())
+
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition",
+            f"attachment; filename={basename(file_path)}",
+        )
+
+        email_message.attach(part)
+
+    text = email_message.as_string()
+
+    with smtplib.SMTP_SSL(
+        smtp_server, smtp_port, context=ssl.create_default_context()
+    ) as email:
+        email.login(sender_email, email_password)
+        email.sendmail(sender_email, receiver_email, text)
+
+
+def main():
+    #number = "7146132874"
+    number = '4693330263'
+    message = "hello !:"
+    provider = "Verizon"
+
+    sender_credentials = ("aleela.gem@gmail.com", str(open("password.txt", "r").read()))
+
+    # SMS
+    #send_sms_via_email(number, message, provider, sender_credentials)
+    #'''
+    # MMS
+    file_path = "gunImages\gunMan.jpg"
+
+    mime_maintype = "image"
+    mime_subtype = "jpeg"
+
+    send_mms_via_email(
+        number,
+        message,
+        file_path,
+        mime_maintype,
+        mime_subtype,
+        provider,
+        sender_credentials,
+    )
+    #'''
+
+
+if __name__ == "__main__":
+    main()
