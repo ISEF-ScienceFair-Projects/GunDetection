ae396400d5add8f3eb4df846efe3769fc1b461aa
Abhirama Sonny <abhirama.sonny@gmail.com>
2023-12-02 15:19:16 -0600

    monkey

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def main():
     config_path_gun = 'gun.cfg'
     yolo_detector = YoloObjD(weight_path_gun, config_path_gun)
 
-    # init
     cam1 = cv2.VideoCapture(0)
     cam2 = cv2.VideoCapture(1)
     cameras = [
@@ -27,21 +26,22 @@ def run_gun_detection(yolo_detector, cameras):
 
     while True:
         combined_frames = []
+        boxes_list = []
 
         for cam, window_name in cameras:
             ret, frame, boxes = run(yolo_detector, cam, window_name)
             if not ret:
                 continue
 
-            combined_frames.append((frame, boxes))
+            combined_frames.append(frame)
+            boxes_list.append(boxes)
 
         if not combined_frames:
             break
 
-        combined_frames_resized = [(cv2.resize(frame, None, fx=2, fy=2), boxes) for frame, boxes in combined_frames]
+        combined_frames_resized = [(cv2.resize(frame, None, fx=2, fy=2), boxes) for frame, boxes in zip(combined_frames, boxes_list)]
         combined_frame = np.hstack([frame for frame, _ in combined_frames_resized])
 
-        # FPS maths
         elapsed_time = time.time() - start_time
         fps = len(cameras) / elapsed_time
         cv2.putText(combined_frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
@@ -55,7 +55,7 @@ def run_gun_detection(yolo_detector, cameras):
             consecutive_gun_detected_count += 1
             if consecutive_gun_detected_count >= buffer_iteration_count:
                 print("Gunman detected for", buffer_iteration_count, "ticks. Clothes detection gonna fire!!")
-                cv2.imwrite("gunImages/gunMan.jpg", combined_frame)    
+                cv2.imwrite("gunImages/gunMan.jpg", combined_frame)
                 run_clothes_detection(cameras)
                 return
         else:
@@ -72,24 +72,31 @@ def run_clothes_detection(cameras):
     models = [Load_DeepFashion2_Yolov3() for _ in range(len(cameras))]
     cap_list = [cv2.VideoCapture(i) for i in range(len(cameras))]
     start_time = time.time()
-    
-    while True:
-        for i, cap in enumerate(cap_list):
-            ret, frame = cap.read()
-            if not ret:
-                print(f"failure on cam{i}")
-                break
 
-            img_tensor = tf.convert_to_tensor(frame[tf.newaxis, ...], dtype=tf.float32) / 255.0
-            img_with_boxes, box_array = Draw_Bounding_Box(frame, Detect_Clothes(img_tensor, models[i]))
+    while True:
+        frames = [cap.read()[1] for cap in cap_list]
+        ret_list = [frame is not None for frame in frames]
+        if not all(ret_list):
+            break
+
+        img_tensors = [tf.convert_to_tensor(frame[tf.newaxis, ...], dtype=tf.float32) / 255.0 for frame in frames]
+        img_with_boxes_list = []
+        box_array_list = []
+
+        for i, (img_tensor, model) in enumerate(zip(img_tensors, models)):
+            img_with_boxes, box_array = Draw_Bounding_Box(frames[i], Detect_Clothes(img_tensor, model))
+            img_with_boxes_list.append(img_with_boxes)
+            box_array_list.append(box_array)
 
             if box_array:
-                avg_rgb_values = get_avg_rgb(box_array, frame)
+                avg_rgb_values = get_avg_rgb(box_array, frames[i])
                 color_category = get_color_category(avg_rgb_values)
                 print(f"Color category for cam {i + 1}: {color_category}")
 
-            elapsed_time = time.time() - start_time
-            fps = len(cameras) / elapsed_time
+        elapsed_time = time.time() - start_time
+        fps = len(cameras) / elapsed_time
+
+        for i, img_with_boxes in enumerate(img_with_boxes_list):
             cv2.putText(img_with_boxes, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.imshow(f"Clothes detection cam {i + 1}", img_with_boxes)
 
