adfa81358f455687d8b882cd507bc4dbf05abd7d
Devaj Shourya Bhogireddi <146690147+MarchEgg@users.noreply.github.com>
2023-12-23 17:27:42 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ def main():
     config_path_gun = 'gun.cfg'
     gun_detection = GunDetection(weight_path_gun, config_path_gun)
 
-    cam1 = cv2.VideoCapture(0)
+    cam1 = cv2.VideoCapture(2)
     cam2 = cv2.VideoCapture(1)
     #cam3 = cv2.VideoCapture(2)
     cameras = [
         (cam1, "Zone 1"), (cam2, 'Zone 2')
     ]
 
-    if gun_detection.run_detection(cameras):
-        clothes_detection = ClothesDetection(len(cameras))
-        clothes_detection.run_detection(cameras)
+    for i in gun_detection.run_detection(cameras):
+        print(f'Zone {i}')
+        if type(i) != int:
+            break
+
+    
+    clothes_detection = ClothesDetection(len(cameras))
+    clothes_detection.run_detection(cameras)
 
 if __name__ == "__main__":
     main()
