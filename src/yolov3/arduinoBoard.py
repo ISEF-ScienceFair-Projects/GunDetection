df9fa1dd7f9f0bd8812b6cb98a69609c28bd754f
Devaj Shourya Bhogireddi <146690147+MarchEgg@users.noreply.github.com>
2023-12-23 19:13:14 -0600

    Update arduinoBoard.py

--- src/yolov3/arduinoBoard.py
+++ src/yolov3/arduinoBoard.py
@@ -1,5 +1,11 @@
 import serial
+ser = serial.Serial('COM7', 9600, timeout=0.5)
+possible_val = [0,1,2,3]
 def sendP(message):
-    ser = serial.Serial('COM7', 9600)
-    val = bytes(message,"utf-8")
-    ser.write(val)
+    if int(message) in possible_val:
+        val = bytes(message,"utf-8")
+        while True:
+            ser.write(val)
+            if str(ser.read(10).decode()) == '5':
+               break
+
